c605d56b8535b35da3e9d52e99a0b3f085931062
QMCBT-JustinEvans <Justin.Ellis.Evans@gmail.com>
2023-01-03 23:17:56 -0600

    ADD Daily warmup; NOT COMPLETE

---
+++ pizza_sql_warmup_answers.sql
@@ -0,0 +1,29 @@
+# SQL Warmup
+# Use the pizza database to answer the following questions.
+
+# What information is stored in the toppings table? 
+SELECT * FROM pizza.toppings;
+# Answer: Toppings, ID index, Price
+
+# How does this table relate to the pizzas table?
+SELECT * FROM pizza.pizzas;
+# Answer: It does not, it needs to be x-referenced using the pizza_toppings table
+
+# What information is stored in the modifiers table? 
+# How does this table relate to the pizzas table?
+
+# How are the pizzas and sizes tables related?
+
+# What other tables are in the database?
+
+# How many unique toppings are there?
+
+# How many unique orders are in this dataset?
+
+# Which size of pizza is sold the most?
+
+# How many pizzas have been sold in total?
+
+# What is the most common size of pizza ordered?
+
+# What is the average number of pizzas per order?
