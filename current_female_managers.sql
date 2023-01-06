dadbcf4872d90962f7761b61a5567695d94e6bcc
QMCBT-JustinEvans <Justin.Ellis.Evans@gmail.com>
2023-01-06 09:38:14 -0600

    UPDATE daily warmup exercise with additional solution method

--- current_female_managers.sql
+++ current_female_managers.sql
@@ -3,4 +3,10 @@ USE employees;
 SELECT first_name, last_name, gender, dept_name FROM employees
 LEFT JOIN dept_manager USING(emp_no)
 LEFT JOIN departments USING(dept_no)
-WHERE to_date LIKE '9999%' and gender = 'F'
+WHERE to_date LIKE '9999%' and gender = 'F';
+
+SELECT first_name, last_name, gender, dept_name FROM employees
+LEFT JOIN dept_manager ON employees.emp_no = dept_manager.emp_no
+	AND to_date > CURDATE()
+    AND gender = 'F'
+LEFT JOIN departments USING(dept_no)
